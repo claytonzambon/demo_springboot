2a419eb17f7ada6e6ce9a1859a9ed3a9409da1f6
Clayton Zambon <zambon@outlook.com.br>
2023-06-07 19:57:38 -0300

    FEAT: Service layer, component registration

--- src/main/java/com/zambon/demo_springboot/resources/UserResource.java
+++ src/main/java/com/zambon/demo_springboot/resources/UserResource.java
@@ -1,19 +1,33 @@
 package com.zambon.demo_springboot.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.zambon.demo_springboot.entities.User;
+import com.zambon.demo_springboot.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Maria", "maria@gmail.com", "49999494949", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
