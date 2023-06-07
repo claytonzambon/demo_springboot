2636d9d94b6e38a58e32aa38113f24a60504adf6
Clayton Zambon <zambon@outlook.com.br>
2023-06-07 13:23:45 -0300

    FEAT: User entity and Resource

---
+++ src/main/java/com/zambon/demo_springboot/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.zambon.demo_springboot.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zambon.demo_springboot.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "maria@gmail.com", "49999494949", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
