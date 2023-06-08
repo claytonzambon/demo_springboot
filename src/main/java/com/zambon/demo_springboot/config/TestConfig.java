50f44502c30082438235cd514d4bce72ac50ee49
Clayton Zambon <zambon@outlook.com.br>
2023-06-07 23:51:54 -0300

    FEAT: Order, Instant, ISO 8601 - Parte 01

--- src/main/java/com/zambon/demo_springboot/config/TestConfig.java
+++ src/main/java/com/zambon/demo_springboot/config/TestConfig.java
@@ -21,7 +21,10 @@ public class TestConfig implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+		User u3 = new User(null, "Bob Brown", "bob@gmail.com", "966666666", "123456");
+		User u4 = new User(null, "Braian Jhonson", "braian@gmail.com", "955555555", "123456");
+		User u5 = new User(null, "Lando Carlrisian", "lando@gmail.com", "944444444", "123456");
 		
-		userRepository.saveAll(Arrays.asList(u1, u2));
+		userRepository.saveAll(Arrays.asList(u1, u2, u3, u4, u5));
 	}	
 }
